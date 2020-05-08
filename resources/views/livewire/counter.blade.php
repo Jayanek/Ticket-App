3398145d1a037561c64af99979f5947d79204f05
Jayanaka <jayanakawka@gmail.com>
2020-05-07 22:22:28 -0700

    livewire-quick-start

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,13 @@
+<div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col"-2><button wire:click="decrement" class="btn btn-danger">-</button></div>
+                <div class="col">{{$count}}</div>
+                <div class="col-2"><button wire:click="increment" class="btn btn-danger">+</button></div>
+            </div>
+        </div>
+    </div>
+
+</div>
